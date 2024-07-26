2288b36c0dda55ad3a29c47381b31588e3f9dba8
moosedev <ahmed.musawir@gmail.com>
2024-07-26 12:26:14 +0800

    26july2024 - clean up

--- app/(superadmin)/layout.tsx
+++ app/(superadmin)/layout.tsx
@@ -1,8 +1,9 @@
 "use client";
 
 import { ReactNode } from "react";
-import Navbar from "@/components/global/Navbar";
 import withSuperAdminProtection from "@/hoc/withSuperAdminProtection";
+import NavbarSuperadmin from "@/components/global/NavbarSuperadmin";
+import Sidebar from "@/components/layout/Sidebar";
 
 interface LayoutProps {
   children: ReactNode;
@@ -11,8 +12,11 @@ interface LayoutProps {
 const SuperAdminLayout = ({ children }: LayoutProps) => {
   return (
     <div className="flex flex-col min-h-screen">
-      <Navbar />
+      <NavbarSuperadmin />
       <div className="flex flex-1">
+        <div className="hidden md:block h-auto flex-shrink-0 border-4 w-[25rem]">
+          <Sidebar />
+        </div>
         <div className="flex-1 p-5 md:max-w-[1140px]">{children}</div>
       </div>
     </div>
